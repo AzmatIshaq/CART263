2993828614c2acc9f5a7d9db4c40bb302d0bf884
Azmat Ishaq <azmatishaq@outlook.com>
2022-02-04 01:43:26 -0500

    Exercise 3 - Spy Profile Plus: Added timer and started bomb diffuse section in animation

--- exercises/spy-profile-generator-plus/js/script.js
+++ exercises/spy-profile-generator-plus/js/script.js
@@ -64,6 +64,25 @@ let greeting;
 let fadeOut = true;
 let startTextAlpha = 0;
 
+// Variable to make bomb diffuse sentence
+
+let bombDiffuseSentence;
+
+// Timer for bomb
+
+// Timer variable for countdown timer
+let timer = {
+  countdown: 30,
+  x: 40,
+  y: 40,
+  textFont: 60
+}
+
+// Variable for typewriter text
+let text1 = `Hello`;
+
+let lastCharacter;
+
 
 /*********************** PRELOAD **********************************************/
 
@@ -152,6 +171,30 @@ function mousePressed() {
 
 function animationState() {
 
+let bombDiffuseSentence = ``
+
+  push();
+  textSize(32);
+  textFont(`Courier, Monospace`);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
+  fill(255);
+  text(`DEFUSE THE BOMB!`, width / 2, height / 6);
+  pop();
+
+  push();
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
+  fill(255);
+  text(`The secret code to diffuse the bomb is`, width / 2, height / 2);
+  text(`${spyProfile.alias} and the ${spyProfile.alias}`, width / 2, height / 1.8);
+  text(text1.substring(0, lastCharacter), width / 2, height / 2.3);
+  pop();
+
+  lastCharacter += 0.1;
+
+  countdownTimer();
 }
 
 /*********************** TITLE STATE ******************************************/
@@ -181,12 +224,12 @@ Password: ${spyProfile.password}`;
 
 
   // Display end winning text
-push();
-fill(255, 255, 255, startTextAlpha);
-textSize(33);
-textAlign(CENTER, CENTER);
-text(`Click For Objective`, width / 2, height / 7);
-pop();
+  push();
+  fill(255, 255, 255, startTextAlpha);
+  textSize(33);
+  textAlign(CENTER, CENTER);
+  text(`Click For Objective`, width / 2, height / 7);
+  pop();
 
 // Fade effect for text
 if (startTextAlpha >= 256 || startTextAlpha <= 0) {
@@ -245,4 +288,37 @@ function setupSpyProfile(data) {
   spyProfile.sidekick = data.sidekick;
 
   spyProfile.password = data.password;
+}
+
+/*********************** COUNTDOWN TIMER **************************************/
+// Function to set up a countdown timer
+
+function countdownTimer() {
+
+  // Give the countdown Timer a background
+
+  push();
+  fill(0);
+  rectMode(CENTER);
+  rect(timer.x, timer.y, 90, 70);
+  pop();
+
+
+  // To display the countdown text
+  push();
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(timer.textFont);
+  text(timer.countdown, timer.x, timer.y);
+  pop();
+
+  // Logic to make the countdown timer operate based on famecount.
+  if (frameCount % 60 == 0 && timer.countdown > 0) {
+    timer.countdown--;
+  }
+
+  // Game over text when countdown reaches 0
+  if (timer.countdown == 0) {
+    state = `endLose`
+  }
 }
