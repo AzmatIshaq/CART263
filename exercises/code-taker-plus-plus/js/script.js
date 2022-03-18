7d097db814a1580d25ecae55eeac94fc8893b2ed
Azmat Ishaq <azmatishaq@outlook.com>
2022-03-18 02:46:08 -0400

    Exercise - Code Taker Plus Plus: Added hint and guide elements

--- exercises/code-taker-plus-plus/js/script.js
+++ exercises/code-taker-plus-plus/js/script.js
@@ -17,8 +17,16 @@ https://jqueryui.com
 
 "use strict";
 
-// The secret answer we're looking for (including capitalization)
-let secret = `This eaten breakfast was delicious`;
+// The secret answers we're looking for (including capitalization)
+//  This eaten breakfast was delicious
+
+// Separated the sentence into variable in order to trigger the alter if
+// the user is on the wrong track
+let secret1 = `This`;
+let secret2 = `This eaten`;
+let secret3 = `This eaten breakfast`;
+let secret4 = `This eaten breakfast was`;
+let secret5 = `This eaten breakfast was delicious`;
 
 // Dialog box to instruct the user on what they should do
 $(`#instructions_dialogue`).dialog({
@@ -33,7 +41,7 @@ $(`#solved-dialog`).dialog({
   autoOpen: false,
   // Add a condescending button to close it
   buttons: {
-    "I know": function() {
+    "I agree": function() {
       $(this).dialog(`close`);
     }
   }
@@ -49,25 +57,56 @@ $(`.secret`).draggable({
   helper: `clone`
 });
 
-// When the user drops a word on the answer...
-$(`#answer`).droppable({
-  drop: function(event, ui) {
-    // Get the word in the dragged element
-    let word = ui.draggable.text();
-    // Add it to the answer box
-    $(this).append(word);
-    // Disable dragging for this word
-    ui.draggable.draggable(`disable`);
-    // Remove the highlighting of this word
-    ui.draggable.removeClass(`found`, 500);
-    // Disable mouseovers on this word
-    ui.draggable.off(`mouseover`);
-    // Check if they got the answer right yet...
-    if ($(`#answer`).text() === secret) {
-      // If they did, display the dialog!
-      $(`#solved-dialog`).dialog(`open`);
+
+  // When the user drops a word on the answer...
+  $(`#answer`).droppable({
+    drop: function(event, ui) {
+      // Get the word in the dragged element
+      let word = ui.draggable.text();
+      // Add it to the answer box
+      $(this).append(word);
+      // Disable dragging for this word
+      ui.draggable.draggable(`disable`);
+      // Remove the highlighting of this word
+      ui.draggable.removeClass(`found`, 500);
+      // Disable mouseovers on this word
+      ui.draggable.off(`mouseover`);
+      // Check if they got the answer right...
+      if ($(`#answer`).text() === secret1 || $(`#answer`).text() === secret2 || $(`#answer`).text() === secret3 || $(`#answer`).text() === secret4) {
+          $(`#keep-going`).dialog(`open`);
+            // alert(`Keep going!`)
+      } else if ($(`#answer`).text() != secret5) {
+        alert(`WRONG! Start over please.`)
+      }
+
+      if ($(`#answer`).text() === secret5){
+        // If they did, display the dialog!
+        $(`#solved-dialog`).dialog(`open`);
+      }
+      // Add a space between the words
+      $(this).append(` `);
     }
-    // Add a space between the words
-    $(this).append(` `);
-  }
+  });
+
+  // Turn the dialog div into an actual dialog
+  $(`#keep-going`).dialog({
+    modal: true,
+    // Don't open it right away
+    autoOpen: false,
+    // Add a condescending button to close it
+    buttons: {
+      "Keep Going!": function() {
+        $(this).dialog(`close`);
+      }
+    }
+  });
+
+
+//  Hint button
+  $(`#button-hint`).on(`click`, function(event) {
+    if ($(`#answer`).text() === ``){
+      alert(`The _`);
+    } else if ((`#answer`).text() === secret1){
+
+    }
 });
