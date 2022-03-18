6bfd4eb513a62d9da2a58d668cd9b174e67f9ba8
Azmat Ishaq <azmatishaq@outlook.com>
2022-03-17 22:24:25 -0400

    Exercise - Code Breaker Plus Plus: Added instructions dialog box

--- exercises/code-taker-plus-plus/js/script.js
+++ exercises/code-taker-plus-plus/js/script.js
@@ -16,6 +16,9 @@ https://jqueryui.com
 // The secret answer we're looking for (including capitalization)
 let secret = `Theremin`;
 
+// Dialog box to instruct the user on what they should do
+$(`#instructions_dialogue`).dialog();
+
 // Turn the dialog div into an actual dialog
 $(`#solved-dialog`).dialog({
   // Don't open it right away
@@ -58,100 +61,3 @@ $(`#answer`).droppable({
     }
   }
 });
-
-/* MY TEMPLATE #4caf50 */
-
-// /* - - - - - - - - - - - VARIABLES - - - - - - - - - - - - - - - - - - - - -  */
-//
-// // Variable to style canvas
-// let canvasProperties = {
-// // Set canvas width and height
-//   w: 850,
-//   h: 550,
-// };
-//
-// // Variable to style the background
-//
-// let bg = {
-//   r: 0,
-//   g: 0,
-//   b: 0,
-// };
-//
-// // Variable to set starting state to `title`
-//
-// let state = `title`;
-//
-//
-// /*********************** PRELOAD **********************************************/
-//
-// /**
-// Description of preload
-// */
-// function preload() {
-//
-// }
-//
-//
-// /*********************** SETUP ************************************************/
-//
-// /**
-// Description of setup
-// */
-// function setup() {
-//
-//   createCanvas(canvasProperties.w, canvasProperties.h);
-//
-// }
-//
-//
-// /*********************** DRAW *************************************************/
-//
-//
-// /**
-// Description of draw
-// */
-// function draw() {
-//   background(bg.r, bg.g, bg.b);
-//
-//     // Alternate between game states
-//   if (state === `title`) {
-//     titleState();
-//   }
-//   if (state === `animation`) {
-//     animationState();
-//   }
-//
-// } // End of draw()
-//
-// /* - - - - - - - - - - - USER INTERACTION - - - - - - - - - - - - - - - - - - */
-//
-// /*********************** KEY PRESSED ******************************************/
-//
-// // function keyPressed() {
-// // }
-//
-// /*********************** MOUSE PRESSED ****************************************/
-//
-// // function mousePressed() {
-// // }
-//
-// /* - - - - - - - - - - - STATES - - - - - - - - - - - - - - - - - - - - - - - */
-//
-// /*********************** ANIMATION STATE **************************************/
-//
-// function animationState() {
-//
-// }
-//
-// /*********************** TITLE STATE ******************************************/
-//
-// function titleState() {
-//
-// }
-//
-// /*********************** RESET STATES *****************************************/
-//
-// function resetStates() {
-//
-// }
