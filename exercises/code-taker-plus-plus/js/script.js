7f59b643add0055b15756742d6a5055efb086c65
Azmat Ishaq <azmatishaq@outlook.com>
2022-03-18 00:31:02 -0400

    Exercise - Code Taker Plus Plus: Updated game color dynamic, and game text

--- exercises/code-taker-plus-plus/js/script.js
+++ exercises/code-taker-plus-plus/js/script.js
@@ -1,9 +1,13 @@
 /**
-Code Taker
-Pippin Barr
-The user can uncover secret letters in the poem by mousing over them. Once found
-they can drag the letters in the correct order to the answer area to find the
-name of an amazing instrument (the theremin) and receive congratulations.
+Code Taker Plus Plus by Azmat Ishaq
+Based on code by Pippin Barr:
+  "The user can uncover secret letters in the poem by mousing over them. Once found
+  they can drag the letters in the correct order to the answer area to find the name of an amazing instrument (the theremin) and receive congratulations."
+
+I altered it to words instead of letters. The words also now have to be discovered by hovering over them. A dialogue modal was also added at the beginning.
+
+  -Azmat
+
 Uses:
 jQuery
 https://jquery.com
@@ -14,7 +18,7 @@ https://jqueryui.com
 "use strict";
 
 // The secret answer we're looking for (including capitalization)
-let secret = `eaten breakfast`;
+let secret = `This eaten breakfast was delicious`;
 
 // Dialog box to instruct the user on what they should do
 $(`#instructions_dialogue`).dialog({
@@ -24,6 +28,7 @@ $(`#instructions_dialogue`).dialog({
 
 // Turn the dialog div into an actual dialog
 $(`#solved-dialog`).dialog({
+  modal: true,
   // Don't open it right away
   autoOpen: false,
   // Add a condescending button to close it
@@ -34,34 +39,35 @@ $(`#solved-dialog`).dialog({
   }
 });
 
-// When the user mouses over secret letters, highlight them
+// When the user mouses over secret words, highlight them
 $(`.secret`).on(`mouseover`, function(event) {
   $(this).addClass(`found`, 500);
 });
 
-// Let the user drag secret letters via a clone helper
+// Let the user drag secret words via a clone helper
 $(`.secret`).draggable({
   helper: `clone`
 });
 
-// When the user drops a letter on the answer...
+// When the user drops a word on the answer...
 $(`#answer`).droppable({
   drop: function(event, ui) {
-    // Get the letter in the dragged element
+    // Get the word in the dragged element
     let word = ui.draggable.text();
     // Add it to the answer box
     $(this).append(word);
-    // Disable dragging for this letter
+    // Disable dragging for this word
     ui.draggable.draggable(`disable`);
-    // Remove the highlighting of this letter
+    // Remove the highlighting of this word
     ui.draggable.removeClass(`found`, 500);
-    // Disable mouseovers on this letter
+    // Disable mouseovers on this word
     ui.draggable.off(`mouseover`);
     // Check if they got the answer right yet...
     if ($(`#answer`).text() === secret) {
       // If they did, display the dialog!
       $(`#solved-dialog`).dialog(`open`);
     }
+    // Add a space between the words
     $(this).append(` `);
   }
 });
