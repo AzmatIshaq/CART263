1bf9eff39be4fab88063532f93e5a5e3212f4934
Azmat Ishaq <azmatishaq@outlook.com>
2022-03-22 21:56:36 -0400

    Exercise: Desperately Seeking Sadness - Added new scene, added fire emoji, and updated avatar emoji

---
+++ exercises/desperately-seeking-sadness-plus-plus/js/Boot.js
@@ -0,0 +1,54 @@
+/**
+A Phaser scene to handle preloading assets before switching to the
+play scene.
+*/
+
+class Boot extends Phaser.Scene {
+
+  /**
+  Just sets the scene's key name
+  */
+  constructor() {
+    super({
+      key: `boot`
+    });
+  }
+
+  /**
+  Loads the image assets then switches to the play scene on completion.
+  */
+  preload() {
+    // Load images
+    this.load.image(`avatar`, `assets/images/avatar.png`);
+    this.load.image(`thumbs-down`, `assets/images/thumbs-down.png`);
+    this.load.image(`thumbs-up`, `assets/images/thumbs-up.png`);
+    this.load.image(`fire-emoji`, `assets/images/fire-emoji.png`);
+    this.load.image(`fire`, `assets/images/fire.png`);
+    // Switch to the play scene on complete
+    this.load.on(`complete`, () => {
+      this.scene.start(`play`);
+    });
+  }
+
+
+
+  create() {
+    // Text styling
+    let style = {
+      fontFamily: 'Arial',
+      fontSize: 64,
+      color: '#ffffff',
+    };
+
+    // Loading text for boot scene
+    let loadingText = `Loading...`;
+    //  Pass in a basic style object with the constructor
+    this.add.text(250, 10, loadingText, style);
+
+  }
+
+  update() {
+
+  }
+
+}
