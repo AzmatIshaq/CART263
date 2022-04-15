a5e9a5817de043ec21e74535479fe226e1036d83
Azmat Ishaq <azmatishaq@outlook.com>
2022-04-15 00:45:54 -0400

    Project 2: Added prelude scene state and image, updated json dialogue,

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -7,24 +7,54 @@ author, and this description to match your project!
 
 Color Palette:
 
-#0D0D0D     (13, 13, 13)
-#0D0D0D     (38, 38, 38)
-#A6826D     (166, 130, 109)
-#BF895A     (191, 137, 90)
-#D99A4E     (217, 154, 78)
-#F2D5A0     (242, 213, 160)
-
-#1e90ff     (30, 144, 255)
+1: #0D0D0D     (13, 13, 13)
+2: #262626     (38, 38, 38)
+3: #A6826D     (166, 130, 109)
+4: #BF895A     (191, 137, 90)
+5: #D99A4E     (217, 154, 78)
+6: #F2D5A0     (242, 213, 160)
+7: #1e90ff     (30, 144, 255)
 
 */
 
 "use strict";
+
+let colorPalette = {
+  r1: 13,
+  g1: 13,
+  b1: 13,
+
+  r2: 38,
+  g2: 38,
+  b2: 38,
+
+  r3: 166,
+  g3: 130,
+  b3: 109,
+
+  r4: 191,
+  g4: 137,
+  b4: 90,
+
+  r5: 217,
+  g5: 154,
+  b5: 78,
+
+  r6: 242,
+  g6: 213,
+  b6: 160,
+
+  r7: 30,
+  g7: 144,
+  b7: 255,
+};
 
 // Venue image variables
 let cafeteriaBgImg;
 let prisonBgImg;
 let hallBgImg;
 let cellBgImg;
+let holePreludeImg;
 
 // Character image variables
 
@@ -40,22 +70,33 @@ let swissArmyImg;
 
 let inventoryImg;
 
+// Inventory Gui properties
+
+let inventoryGui = {
+  x: 2,
+  y: 2,
+  width: 650,
+  height: 360,
+  alpha: 210,
+};
+
 // Item position
 
 let ramen = {
-  x: 850 / 5,
-  y: 400 /2,
-  xInventory: 100,
-  yInventory: 100,
+  x: 5,
+  y: 2,
+  xInventory: 2,
+  yInventory: 2,
   width: 50,
   height: 50,
   size:  50
-}
+};
 
 // states
 // let state = `title`;
 // let state = ``;
-let state = `sceneOneCafeteria`;
+let state = `sceneOnePrelude`;
+// let state = `sceneOneCafeteria`;
 
 // Inventory variables
 // Tracks if inventory interface is open
@@ -73,13 +114,17 @@ let tradeActive = true;
 
 // Variables to verify trade completion between scenes
 
-let tradeCompleteSceneOne = false;
-let tradeCompleteSceneTwo = false;
-let tradeCompleteSceneThree = false;
+let tradesCompleteSceneOne = false;
+let tradesCompleteSceneTwo = false;
+let tradesCompleteSceneThree = false;
 
 // Variables to manage item collection
-
 let ramenCollected = false;
+let gumCollected = true;
+
+// Variable to register ramen trade
+
+let ramenTraded = false;
 
 /**
 Description of preload
@@ -91,6 +136,7 @@ function preload() {
   prisonBgImg = loadImage('assets/images/prison_main.png');
   hallBgImg = loadImage('assets/images/prison-hall.jpg');
   cellBgImg = loadImage('assets/images/prison-cell-scene.jpg');
+  holePreludeImg = loadImage('assets/images/hole_1_filter_night.jpg');
 
   // Preload character images
   wardenCharImg = loadImage('assets/images/warden.png');
@@ -120,7 +166,8 @@ function setup() {
 
   // jQuery
   // Make the trade items draggable.
-  $(".items").draggable({
+  $(".my-items").draggable({
+     containment: "#trade-container"
     });
 
   $(`#trade-screen-2`).droppable({
@@ -163,24 +210,33 @@ function draw() {
 // Function to manage trades
 
 function makeTrade(event, ui) {
+
+  // Succesful trades for Scene One Cafeteria
   if (state === `sceneOneCafeteria`) {
     if (ui.draggable.attr(`id`) === `ramen`) {
-      alert(`successful trade!`);
-
+      alert(`Successful trade! Alright Teddy, you got a deal.`);
 
       // Register completed trade
-      tradeCompleteSceneOne = true;
+      tradesCompleteSceneOne = true;
+
+      // Register item trade
+      ramenTraded = true;
+      gumCollected = true;
 
       // Successful trade!
       // Add something cool to the player's inventory
       // Display a message(s) from the cellmate
-      $("#gum-trader").hide();
-      $("#gum-player").show();
-      $(".items").draggable("disable");
+      $("#gum-player").removeClass("hidden-item");
+      $("#gum-player").addClass("active-item");
+      $("#gum-trader").removeClass("active-item");
+      $("#gum-trader").addClass("hidden-item");
+      // $("#gum-trader").hide();
+      // $("#gum-player").show();
+      $(".my-items").draggable("disable");
     }
     else {
       // Unsuccessful trade!
-      alert(`Trade rejected! Hey fool how about something I actually need?`);
+      alert(`Trade rejected! Hey fool, how about something I actually need?`);
     }
   }
   else if (state === `dining`) {
@@ -204,17 +260,17 @@ function titleState() {
   image(prisonBgImg, 0, 0, 550, 400);
 }
 
+// Function to manage trades for each scene
 function sceneOne() {
 
   if (tradeActive === true && state === `sceneOneCafeteria`) {
-
       // Will utilize this for scene switching and event triggering as well
 
       $(`#go-to-dining`).on(`click`, function() {
         state = `dining`;
       });
 
-      if (tradeCompleteSceneOne) {
+      if (tradesCompleteSceneOne) {
         state = `sceneTwo`;
       }
   }
@@ -246,8 +302,20 @@ function sceneTwo() {
 
 }
 
-
 function animation() {
+
+  if (state === `sceneOnePrelude`) {
+    push();
+    image(holePreludeImg, 0, 0, width / 1.54, height)
+    pop();
+
+    push();
+    fill(colorPalette.r3, colorPalette.g3, colorPalette.b3);
+    textSize(18);
+    text(gameTextData.sceneOne.prelude, width / 1.45, height / 9.5, 250, 350 );
+    pop();
+  }
+
 
   // Animation for sceneOne
   if (state === `sceneOneCafeteria`) {
@@ -256,11 +324,11 @@ function animation() {
     image(cafeteriaBgImg, 0, 0, 550, 400);
 
     // If the ramen hasn't been collected don't display it
-    if (ramenCollected === false) {
+    if (ramenCollected === false ) {
     // Ramen Img
     push();
     imageMode(CENTER);
-    image(ramenImg, ramen.x, ramen.y, ramen.width, ramen.height)
+    image(ramenImg, width / ramen.x, height / ramen.y, ramen.width, ramen.height)
     pop();
     }
 
@@ -302,7 +370,7 @@ function mouseClicked() {
 
   if (state === `sceneOneCafeteria` && ramenCollected === false) {
 
-    if (dist(mouseX, mouseY, ramen.x, ramen.y) <= ramen.size / 2) {
+    if (dist(mouseX, mouseY, width / ramen.x, height / ramen.y) <= ramen.size / 2) {
       // alert(`hello`);
       ramenCollected = true;
       // $( `#ramen` ).switchClass( "hidden-item", "active-item");
@@ -316,18 +384,19 @@ function inventory() {
 
   if (inventoryActive === true && inventoryDisplay === true) {
 
-    push();
-    rectMode(CENTER);
-    noStroke();
-    fill(190, 50, 20)
-    rect(30, 20, 55, 55);
-    pop();
+      // Inventory interface
+      push();
+      rectMode(CENTER);
+      noStroke();
+      fill(colorPalette.r3, colorPalette.g3, colorPalette.b3, inventoryGui.alpha);
+      rect(width / inventoryGui.x, height / inventoryGui.y, inventoryGui.width, inventoryGui.height);
+      pop();
 
-
-    if (ramenCollected === true) {
+    // Items to display in inventory
+    if (ramenCollected === true && ramenTraded === false) {
       push();
       imageMode(CENTER);
-      image(ramenImg, ramen.x, ramen.y, ramen.width, ramen.height)
+      image(ramenImg, width / ramen.xInventory, height / ramen.yInventory, ramen.width, ramen.height)
       pop();
       }
     }
@@ -341,4 +410,14 @@ function keyPressed() {
   } else {
     inventoryDisplay = false;
   }
+}
+
+
+// Function to manage scene completions
+function sceneComplete() {
+
+  if (tradesCompleteSceneOne) {
+
+  }
+
 }
