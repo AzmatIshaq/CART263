21c527cd08b73fa2e78766ab8838cc12b8b3d175
Azmat Ishaq <azmatishaq@outlook.com>
2022-04-15 23:19:56 -0400

    Project 2: Updated scene states, debugging JSON, updated game mechanics

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -15,35 +15,50 @@ Color Palette:
 6: #F2D5A0     (242, 213, 160)
 7: #1e90ff     (30, 144, 255)
 
+Characters:
+
+image(wardenCharImg, 620, 10, 150, 150);
+image(smokeyCharImg, 620, 10, 150, 150);
+
+
 */
 
 "use strict";
 
+// Color Palette with rgb properties
 let colorPalette = {
+
+// #0D0D0D     (13, 13, 13)
   r1: 13,
   g1: 13,
   b1: 13,
 
+// #262626     (38, 38, 38)
   r2: 38,
   g2: 38,
   b2: 38,
 
+// #A6826D     (166, 130, 109)
   r3: 166,
   g3: 130,
   b3: 109,
 
+// #BF895A     (191, 137, 90)
   r4: 191,
   g4: 137,
   b4: 90,
 
+// #D99A4E     (217, 154, 78)
   r5: 217,
   g5: 154,
   b5: 78,
 
+// #F2D5A0     (242, 213, 160)
   r6: 242,
   g6: 213,
   b6: 160,
 
+// #1e90ff     (30, 144, 255)
   r7: 30,
   g7: 144,
   b7: 255,
@@ -57,17 +72,15 @@ let cellBgImg;
 let holePreludeImg;
 
 // Character image variables
-
 let wardenCharImg;
+let smokeyCharImg;
 
 // Items image variables
-
 let ramenImg;
 let gumImg;
 let swissArmyImg;
 
 // Misc image variables
-
 let inventoryImg;
 
 // Inventory Gui properties
@@ -95,8 +108,8 @@ let ramen = {
 // states
 // let state = `title`;
 // let state = ``;
-let state = `sceneOnePrelude`;
-// let state = `sceneOneCafeteria`;
+// let state = `sceneOnePrelude`;
+let state = `sceneOneCafeteria`;
 
 // Inventory variables
 // Tracks if inventory interface is open
@@ -105,11 +118,45 @@ let inventoryActive = true;
 let inventoryDisplay = false;
 
 // Variable for JSON data
-
 let gameTextData;
 
-// Variables to activate trade moments
+// Variable to manage display dialogue properties
+let dialogDisplay = {
+  size: 16,
+  playerX: 580,
+  playerY: 50,
+  playerWidth: 250,
+  playerHeight: 350,
+  npcX: 1.45,
+  npcY: 2,
+  npcWidth: 250,
+  npcHeight: 350
+}
 
+// Variable to manage display character properties
+let charDisplay = {
+  x: 620,
+  y: 10,
+  width: 150,
+  height: 150
+}
+
+// Variable to manage background images
+let bgDisplay = {
+  x: 0,
+  y: 0,
+  width: 550,
+  height: 400
+}
+
+
+// Variable to manage JSON for the npc dialogue that is active
+let activeNpcDialog;
+// Variable to manage JSON for player dialogue that is active
+let activePlayerDialog;
+
+
+// Variables to activate trade moments
 let tradeActive = true;
 
 // Variables to verify trade completion between scenes
@@ -120,11 +167,12 @@ let tradesCompleteSceneThree = false;
 
 // Variables to manage item collection
 let ramenCollected = false;
-let gumCollected = true;
+let gumCollected = false;
 
 // Variable to register ramen trade
 
 let ramenTraded = false;
+let gumTraded = false;
 
 /**
 Description of preload
@@ -137,6 +185,7 @@ function preload() {
   hallBgImg = loadImage('assets/images/prison-hall.jpg');
   cellBgImg = loadImage('assets/images/prison-cell-scene.jpg');
   holePreludeImg = loadImage('assets/images/hole_1_filter_night.jpg');
+  smokeyCharImg = loadImage('assets/images/smokey_inmate_night.jpg');
 
   // Preload character images
   wardenCharImg = loadImage('assets/images/warden.png');
@@ -147,7 +196,6 @@ function preload() {
   swissArmyImg = loadImage('assets/images/swiss_army.png');
 
   // Preload inventory images
-  // inventoryImg = loadImage(`assets/images/`)
 
   // Preload JSON game text
   //
@@ -173,10 +221,7 @@ function setup() {
   $(`#trade-screen-2`).droppable({
     drop: makeTrade
   });
-
-
 }
-
 
 /**
 To display and run the scene events and animations.
@@ -212,7 +257,7 @@ function draw() {
 function makeTrade(event, ui) {
 
   // Succesful trades for Scene One Cafeteria
-  if (state === `sceneOneCafeteria`) {
+  if (state === `sceneOneCafeteria` && tradeActive === true) {
     if (ui.draggable.attr(`id`) === `ramen`) {
       alert(`Successful trade! Alright Teddy, you got a deal.`);
 
@@ -226,10 +271,12 @@ function makeTrade(event, ui) {
       // Successful trade!
       // Add something cool to the player's inventory
       // Display a message(s) from the cellmate
+
       $("#gum-player").removeClass("hidden-item");
       $("#gum-player").addClass("active-item");
       $("#gum-trader").removeClass("active-item");
       $("#gum-trader").addClass("hidden-item");
+
       // $("#gum-trader").hide();
       // $("#gum-player").show();
       $(".my-items").draggable("disable");
@@ -239,7 +286,7 @@ function makeTrade(event, ui) {
       alert(`Trade rejected! Hey fool, how about something I actually need?`);
     }
   }
-  else if (state === `dining`) {
+  else if (state === `sceneTwoCell`) {
     if (ui.draggable.attr(`id`) === `spork`) {
       // Successful trade!
       // Add something cool to the player's inventory
@@ -252,7 +299,6 @@ function makeTrade(event, ui) {
     }
   }
 }
-
 
 /* - - - - - - - - - - - STATES - - - - - - - - - - - - - - - - - - - - - -  */
 
@@ -275,13 +321,10 @@ function sceneOne() {
       }
   }
 
-
   // Scene One - Hall
 
       if (state === `sceneOneHall`) {
           image(hallBgImg, 0, 0, 550, 400);
-
-          image(wardenCharImg, 620, 10, 150, 150);
 
           push();
           fill(255);
@@ -311,7 +354,7 @@ function animation() {
 
     push();
     fill(colorPalette.r3, colorPalette.g3, colorPalette.b3);
-    textSize(18);
+    textSize(16);
     text(gameTextData.sceneOne.prelude, width / 1.45, height / 9.5, 250, 350 );
     pop();
   }
@@ -320,24 +363,44 @@ function animation() {
   // Animation for sceneOne
   if (state === `sceneOneCafeteria`) {
 
-    // Scene Venue image
-    image(cafeteriaBgImg, 0, 0, 550, 400);
+      // Player dialog text
+      push();
+      fill(255);
+      textSize(dialogDisplay.size);
+      text(activePlayerDialog, dialogDisplay.playerX, dialogDisplay.playerY, dialogDisplay.playerWidth, dialogDisplay.playerHeight);
+      pop();
 
-    // If the ramen hasn't been collected don't display it
-    if (ramenCollected === false ) {
-    // Ramen Img
-    push();
-    imageMode(CENTER);
-    image(ramenImg, width / ramen.x, height / ramen.y, ramen.width, ramen.height)
-    pop();
+      // Npc dialog text
+      push();
+      fill(255);
+      textSize(dialogDisplay.size);
+      text(activeNpcDialog, width / dialogDisplay.npcX, height / dialogDisplay.npcY, dialogDisplay.npcWidth, dialogDisplay.npcHeight);
+      pop();
+
+      // Scene Venue image
+      image(cafeteriaBgImg, bgDisplay.x, bgDisplay.y, bgDisplay.width, bgDisplay.height);
+
+      // Ramen trade event
+      if (ramenCollected === false ) {
+
+      // Starting player dialogue
+      let activePlayerDialog = gameTextData.sceneOne.cafeteria[0].player
+
+      // Ramen Img
+      push();
+      imageMode(CENTER);
+      image(ramenImg, width / ramen.x, height / ramen.y, ramen.width, ramen.height)
+      pop();
+
+      }
+
+// Trade Scene Dialogue
+    if (ramenCollected) {
+    let activeNpcDialog = gameTextData.sceneOne.cafeteria[1].inmateSmokey;
+      push();
+      image(smokeyCharImg, charDisplay.x, charDisplay.y, charDisplay.width, charDisplay.height);
+      pop();
     }
-
-    // Scene Dialogue
-    push();
-    fill(255);
-    textSize(12);
-    text(gameTextData.sceneOne.cafeteria, 580, 40);
-    pop();
   }
 
   if (state === `sceneOneHall`) {
@@ -399,10 +462,21 @@ function inventory() {
       image(ramenImg, width / ramen.xInventory, height / ramen.yInventory, ramen.width, ramen.height)
       pop();
       }
+
+      if (gumCollected === true && gumTraded === false) {
+        push();
+        imageMode(CENTER);
+        image(gumImg, width / gum.xInventory, height /gum.yInventory, gum.width, gum.height)
+        pop();
+        }
     }
 }
 
 function keyPressed() {
+
+  if (state === `sceneOnePrelude` && keyCode === ENTER) {
+    state = `sceneOneCafeteria`;
+  }
 
   // Display inventory
   if (state === `sceneOneCafeteria` && keyCode === 73 && inventoryDisplay === false) {
@@ -419,5 +493,11 @@ function sceneComplete() {
   if (tradesCompleteSceneOne) {
 
   }
+
+}
+
+// Function to manage characters
+
+function characters(name) {
 
 }
